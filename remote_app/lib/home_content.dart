b38bb2c2e508ee00340e5fe4d132e75eeefd92b9
vod32585 <david1.vollmer@st.oth-regensburg.de>
2024-06-11 21:30:19 +0200

    Added some Widget functionality

--- remote_app/lib/home_content.dart
+++ remote_app/lib/home_content.dart
@@ -6,14 +6,51 @@ class HomeContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final buttonWidth =
+        screenSize.width / 3; // Divide screen width into 3 equal parts
+    final buttonHeight =
+        screenSize.height / 3; // Divide screen height into 3 equal parts
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 212, 230, 25),
-        title: const Text('Remote'),
+        title: const Text('Spiegel AI Remote'),
       ),
       endDrawer: const AppDrawer(),
-      body: const Center(
-        child: Text('Remote Content'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.count(
+          crossAxisCount: 3,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          children: List.generate(
+            9,
+            (index) => SizedBox(
+              width: buttonWidth,
+              height: buttonHeight,
+              child: Card(
+                key: ValueKey(index),
+                color: const Color.fromARGB(255, 235, 235, 235),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  side: const BorderSide(
+                    color: Colors.black,
+                    width: 1.0,
+                  ),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    // Handle button tap
+                  },
+                  child: Center(
+                    child: Text('Widget ${index + 1}'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- remote_app/lib/widgets_content.dart
+++ remote_app/lib/widgets_content.dart
@@ -1,8 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:remote_app/drawer.dart';
+import 'package:reorderables/reorderables.dart';
 
-class WidgetsContent extends StatelessWidget {
+class WidgetsContent extends StatefulWidget {
   const WidgetsContent({super.key});
+
+  @override
+  State<WidgetsContent> createState() => _WidgetsContentState();
+}
+
+class _WidgetsContentState extends State<WidgetsContent> {
+  late List<Widget> _items;
+
+  @override
+  void initState() {
+    super.initState();
+    _setLandscapeOrientation();
+    _items = List<Widget>.generate(9, (index) {
+      return _buildGridItem(index);
+    });
+  }
+
+  Widget _buildGridItem(int index) {
+    return Card(
+      key: ValueKey(index),
+      color: Colors.amber,
+      child: InkWell(
+        onTap: () {
+          // Handle the tap event here
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Item $index clicked')),
+          );
+        },
+        child: Center(
+          child: Text('Item $index'),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _resetOrientation();
+    super.dispose();
+  }
+
+  void _setLandscapeOrientation() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+
+  void _resetOrientation() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +68,15 @@ class WidgetsContent extends StatelessWidget {
         title: const Text('Widgets'),
       ),
       endDrawer: const AppDrawer(),
-      body: const Center(
-        child: Text('Widgets Content'),
+      body: ReorderableWrap(
+        needsLongPressDraggable: false,
+        onReorder: (int oldIndex, int newIndex) {
+          setState(() {
+            final item = _items.removeAt(oldIndex);
+            _items.insert(newIndex, item);
+          });
+        },
+        children: _items,
       ),
     );
   }
