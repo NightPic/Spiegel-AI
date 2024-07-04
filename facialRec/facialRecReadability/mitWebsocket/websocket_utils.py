0a0874113cacb243c6a0a2c928c10201a400459f
Marco Kuner <kum40001@hs-regensburg.de>
2024-07-04 15:05:05 +0200

    ADDED einmaliges senden bei Detection und fetch

--- facialRec/facialRecReadability/mitWebsocket/websocket_utils.py
+++ facialRec/facialRecReadability/mitWebsocket/websocket_utils.py
@@ -1,42 +1,44 @@
 import json
 import threading
-import websocket
+from websocket import create_connection, WebSocketApp
 from profile_utils import load_profiles
+
+ws_app = None
+ws_app_url = "ws://localhost:8000"
 
 def on_message(ws, message):
     message_data = json.loads(message)
     sender = message_data.get("sender")
     if sender == "remote":
         profiles = message_data.get("profiles")
-        with open("../../../Display/profiles.json", "w") as json_file:
+        with open("profiles.json", "w") as json_file:
             json.dump(profiles, json_file, indent=4)
     elif sender == "fetch":
         profiles = load_profiles()
-        ws.send(json.dumps({"sender": "mirror", "profiles": profiles}))
+        ws.send(json.dumps({"sender": "spiegel", "profiles": profiles}))
 
 def on_error(ws, error):
     print(error)
 
-def on_close(ws):
+def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
 
 def on_open(ws):
     print("### connected ###")
-    profiles = load_profiles()
-    ws.send(json.dumps({"sender": "mirror", "profiles": profiles}))
 
 def start_websocket():
-    ws = websocket.WebSocketApp("ws://192.168.178.42:8000",
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+    global ws_app
+    ws_app = WebSocketApp(ws_app_url,
+                          on_message=on_message,
+                          on_error=on_error,
+                          on_close=on_close)
+    ws_app.on_open = on_open
+    ws_app.run_forever()
 
 def send_profiles(profiles):
-    ws = websocket.create_connection("ws://192.168.178.42:8000")
-    ws.send(json.dumps({"sender": "mirror", "profiles": profiles}))
-    ws.close()
+    global ws_app
+    if ws_app and ws_app.sock.connected:
+        ws_app.send(json.dumps({"sender": "spiegel", "profiles": profiles}))
 
 # Start WebSocket thread
 def initiate_websocket_connection():
